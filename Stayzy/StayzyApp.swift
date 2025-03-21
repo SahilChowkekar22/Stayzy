e83b0a1fb4816bcbbdd64dd7d5644d2f9d8b9039
SahilChowkekar22 <Sahil.Chowkekar@techconsulting.tech>
2025-03-21 10:39:11 -0400

    Initial Commit

---
+++ Stayzy/StayzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayzyApp.swift
+//  Stayzy
+//
+//  Created by Sahil ChowKekar on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct StayzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
